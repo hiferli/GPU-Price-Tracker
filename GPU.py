275e12f41b90e5c43aa3349420fc488ad27945af
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-14 14:19:15 +0530

    Getting HTML Document

--- GPU.py
+++ GPU.py
@@ -3,4 +3,12 @@ import requests
 import re
 
 # Asking User Preferences
-graphics = input("What kind of product are you looking for?")
+# graphics = input("What kind of product are you looking for?")
+graphics = 3080
+
+# Getting the URL
+url = f"https://www.newegg.ca/p/pl?d={graphics}&N=4131"
+
+# Getting the HTML Document using BeautifulSoup
+document = BeautifulSoup(requests.get(url).text , 'html.parser');
+
