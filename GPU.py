a6de800cace66da44b77905431ba7e0d05ae348b
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-14 14:35:29 +0530

    Getting Links to the GPU Buy Pages

--- GPU.py
+++ GPU.py
@@ -21,3 +21,18 @@ lastPageNumber = int(str(nextPages).split('/')[-2].split(">")[-1][:-1]);
 for page in range(1 , lastPageNumber + 1):
     url = f"https://www.newegg.ca/p/pl?d={graphics}&N=4131&page={page}"
     pageDocument = BeautifulSoup(requests.get(url).text , 'html.parser') 
+
+    # Div with all the Graphic Card Information
+    cardsDiv = pageDocument.find(class_ = "item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+    # Getting info from all the contents
+    cards = cardsDiv.find_all(text = re.compile(str(graphics)));
+
+    # Traversing through all the GPUs available in the search
+    for card in cards:
+        cardParent = card.parent;
+
+        if cardParent.name != 'a':
+            continue;
+
+        link = cardParent['href'];
+        print(link);
